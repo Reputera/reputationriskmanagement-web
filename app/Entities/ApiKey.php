5cf6a7105a39530a9887850f8ed29aa5b4751f37
Terry Rowland <terry.enolalabs.com>
2016-02-23 14:29:26 -0500

    Created the entity and migration for the api_keys table for protecting the app from public access.

---
+++ app/Entities/ApiKey.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Entities;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+/**
+ * App\Entities\ApiKey
+ *
+ * @property integer $id
+ * @property string $username
+ * @property string $key
+ * @property \Carbon\Carbon $created_at
+ * @property \Carbon\Carbon $updated_at
+ * @property \Carbon\Carbon $deleted_at
+ * @method static \App\Entities\ApiKey|null find($id)
+ * @method static \Illuminate\Database\Query\Builder|\App\Entities\ApiKey whereId($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\Entities\ApiKey whereUsername($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\Entities\ApiKey whereKey($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\Entities\ApiKey whereCreatedAt($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\Entities\ApiKey whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\Entities\ApiKey whereDeletedAt($value)
+ */
+class ApiKey extends Model
+{
+    use SoftDeletes;
+
+    protected $dates = ['created_at', 'updated_at', 'deleted_at'];
+}
