6a75c4622fe8404feab5ccd7db74e1e076e4977c
Terry Rowland <terry.enolalabs.com>
2016-03-02 10:01:16 -0500

    Created a seeder for an admin user.

---
+++ database/seeds/UserTableSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+use App\Entities\User;
+use Illuminate\Database\Seeder;
+
+class UserTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::create([
+            'name' => 'Admin',
+            'email' => 'admin@rrm.com',
+            'password' => bcrypt('testing'),
+        ]);
+    }
+}
