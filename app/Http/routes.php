223f555f0ca05ed0b4b60946dc1f24adc9f6af67
Terry Rowland <terry.enolalabs.com>
2016-03-02 10:00:53 -0500

    Created the routes needed for logging in and logging out, along with a route that will be used for landing.

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,5 +1,18 @@
 <?php
 
+Route::group(['middleware' => ['web']], function () {
+    Route::get('login', 'Auth\AuthController@getLogin');
+    Route::post('login', 'Auth\AuthController@postLogin');
+
+    Route::group(['middleware' => ['auth']], function () {
+        Route::get('/', function () {
+            return view('layouts.default');
+        })->name('admin.landing');
+    });
+
+    Route::get('logout', 'Auth\AuthController@logout')->name('logout');
+});
+
 Route::group(['prefix' => 'api', 'middleware' => ['api']], function () {
     Route::post('login', 'Auth\ApiAuthController@authenticate')->name('api.post.login');
 
