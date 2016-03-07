96a16dead1a1b3c5b3f67841e8d27243fe5b0509
Terry Rowland <terry.enolalabs.com>
2016-03-07 10:16:17 -0500

    Created a function to get hint the return type via comments. Also added some defaults that we have used in the past for requests.

--- app/Http/Requests/Request.php
+++ app/Http/Requests/Request.php
@@ -6,5 +6,21 @@ use Illuminate\Foundation\Http\FormRequest;
 
 abstract class Request extends FormRequest
 {
-    //
+    public function authorize()
+    {
+        return true;
+    }
+
+    public function rules()
+    {
+        return [];
+    }
+
+    /**
+     * @return \App\Entities\User
+     */
+    public function user($guard = null)
+    {
+        return parent::user();
+    }
 }
