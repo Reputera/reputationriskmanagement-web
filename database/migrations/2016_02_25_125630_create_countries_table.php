9ce642238b0c5c99ec69b10d2581393aaf5ac62f
Terry Rowland <terry.enolalabs.com>
2016-03-17 09:41:30 -0400

    I'm not sure if the field should be empty, but I'm going to side on caution and make it so.

--- database/migrations/2016_02_25_125630_create_countries_table.php
+++ database/migrations/2016_02_25_125630_create_countries_table.php
@@ -15,7 +15,7 @@ class CreateCountriesTable extends Migration
         Schema::create('countries', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name')->index();
-            $table->string('entity_id');
+            $table->string('entity_id')->nullable();
             $table->unsignedInteger('region_id')->nullable();
             $table->timestamps();
 
