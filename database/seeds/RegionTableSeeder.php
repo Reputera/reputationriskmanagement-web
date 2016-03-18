27017799b93b4a5fe867cf72f2f173ccf034cfa7
Terry Rowland <terry.enolalabs.com>
2016-03-18 08:46:05 -0400

    Decided to force the IDs so there's no weird things happening in MySQL when the Countries Seeder relies on the IDs.

--- database/seeds/RegionTableSeeder.php
+++ database/seeds/RegionTableSeeder.php
@@ -13,38 +13,47 @@ class RegionTableSeeder extends Seeder
     public function run()
     {
         Region::create([
+            'id' => 1,
             'name' => 'Asia',
             'entity_id' => 'B_Fa4'
         ]);
         Region::create([
+            'id' => 2,
             'name' => 'Asia and east Africa',
             'entity_id' => 'HwAhdb'
         ]);
         Region::create([
+            'id' => 3,
             'name' => 'Antarctica',
             'entity_id' => 'CAvQc'
         ]);
         Region::create([
+            'id' => 4,
             'name' => 'Oceania',
             'entity_id' => 'Bch5HB'
         ]);
         Region::create([
+            'id' => 5,
             'name' => 'Africa',
             'entity_id' => 'B_FGS'
         ]);
         Region::create([
+            'id' => 6,
             'name' => 'Europe',
             'entity_id' => 'B_FA-'
         ]);
         Region::create([
+            'id' => 7,
             'name' => 'South America',
             'entity_id' => 'B_KvE'
         ]);
         Region::create([
+            'id' => 8,
             'name' => 'Europe; Asia',
             'entity_id' => 'I_aUUN'
         ]);
         Region::create([
+            'id' => 9,
             'name' => 'North America',
             'entity_id' => 'B_GRZ'
         ]);
