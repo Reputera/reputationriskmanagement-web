39c6b02195deafbc9b2cd054cfe204e46cb77779
Terry Rowland <terry.enolalabs.com>
2016-03-21 07:46:39 -0400

    Removed the country function since an instance can include or effect several countries. This also lead to the fact we need to be able to put "related entities" on an instance so we can look at them to determine the country. Also added in some comments for cleanliness.

--- app/Services/Vendors/RecordedFuture/Instance.php
+++ app/Services/Vendors/RecordedFuture/Instance.php
@@ -4,43 +4,85 @@ namespace App\Services\Vendors\RecordedFuture;
 
 class Instance extends BaseRecord
 {
+    /**
+     * @var array
+     */
+    protected $relatedEntities = [];
+
+    /**
+     * Get the Recorded Future instance ID.
+     *
+     * @return string
+     */
     public function getId(): string
     {
         return $this->getFieldAsString('id');
     }
 
+    /**
+     * Get the instance type.
+     *
+     * @return string
+     */
     public function getType(): string
     {
         return $this->getFieldAsString('type');
     }
 
+    /**
+     * Get the fragment.
+     *
+     * @return string
+     */
     public function getFragment(): string
     {
         return $this->getFieldAsString('fragment');
     }
 
+    /**
+     * Get the Document object.
+     *
+     * @return Document
+     */
     public function getDocument(): Document
     {
         return new Document(array_get($this->record, 'document'));
     }
 
+    /**
+     * Get the attributes.
+     *
+     * @return InstanceAttributes
+     */
     public function getAttributes(): InstanceAttributes
     {
         return new InstanceAttributes(array_get($this->record, 'attributes'));
     }
 
-    public function getCountry(): string
+    /**
+     * Adds entities that are involved in the instance.
+     *
+     * @param array $entities
+     */
+    public function setRelatedEntities(array $entities)
     {
-        if ($country = $this->getDocument()->getSource()->getCountry()) {
-            return $country;
-        }
+        $this->relatedEntities = $entities;
+    }
 
-        foreach ($this->getFieldAsArray('entities') as $entity) {
-            if (array_get($entity, 'type') == 'Country' && array_has($entity, 'name')) {
-                return array_get($entity, 'name');
+    /**
+     * Gets all the entities that are countries.
+     *
+     * @return array
+     */
+    public function getCountries(): array
+    {
+        $countries = [];
+        foreach ($this->relatedEntities as $entity) {
+            if ($entity->getType() == 'Country' && $entity->getName()) {
+                $countries[] = $entity;
             }
         }
 
-        return '';
+        return $countries;
     }
 }
--- tests/Unit/Services/Vendors/RecordedFuture/InstanceTest.php
+++ tests/Unit/Services/Vendors/RecordedFuture/InstanceTest.php
@@ -3,104 +3,24 @@
 namespace Tests\Unit\Services\Vendors\RecordedFuture;
 
 use App\Services\Vendors\RecordedFuture\Document;
+use App\Services\Vendors\RecordedFuture\Entity;
 use App\Services\Vendors\RecordedFuture\Instance;
 use App\Services\Vendors\RecordedFuture\InstanceAttributes;
-use App\Services\Vendors\RecordedFuture\RecordedFutureApi;
+use Tests\StubData\RecordedFuture\SingleInstance;
 
 class InstanceTest extends \TestCase
 {
     /** @var Document */
     protected $instance;
 
-    protected $fields = [
-        'id' => 'GUwp9aBfdDn',
-        'type' => 'RFEVEOrganizationRelationship',
-        'cluster_id' => 'C3poAeCXtyk',
-        'cluster_ids'=>[
-            'C3poAeCXtyk'
-        ],
-        'start' => '2015-03-10T23:25:08.000Z',
-        'stop' => '2015-03-10T23:25:08.000Z',
-        'tagged_fragment' => '$has:<i id=GUwp9aBfdDn><e id=KHV7oS>Us Hasbro</e> in buy area, gives stockholders 2-for-1 deal <e id=KerzW8>#Hasbro</e></i> Inc <e id=url:http://tinyurl.com/pohcymh>http://t.co/GZqj2hW9wi</e> <e id=KezJ4l>#sp500</e>.',
-        'fragment' => '$has:Us Hasbro in buy area, gives stockholders 2-for-1 deal #Hasbro Inc http://t.co/GZqj2hW9wi #sp500.',
-        'item_fragment' => 'Us Hasbro in buy area, gives stockholders 2-for-1 deal #Hasbro',
-        'precision' => 'ms',
-        'time_type' => 'in',
-        'document'=> [
-            'id' => 'NpIPGf',
-            'title' => '$has:Us Hasbro in buy area, gives stockholders 2-for-1 deal #Hasbro Inc http://t.co/GZqj2hW9wi #sp500',
-            'language' => 'eng',
-            'published' => '2015-03-11T15:27:22.000Z',
-            'downloaded' => '2015-03-11T15:27:22.831Z',
-            'indexed' => '2015-03-11T15:27:22.000Z',
-            'url' => 'https://twitter.com/FinSentS_SP500/status/575679412219330560',
-            'sourceId'=> [
-                'id' => 'BV5',
-                'name' => 'Twitter',
-                'description' => 'Twitter',
-                'media_type' => 'JxSEtC',
-                'country' => 'United States'
-            ]
-        ],
-        'attributes'=> [
-            'general_negative'=>0,
-            'function' => 'id',
-            'user_data' => [
-                'friends_count'=>1518,
-                'statuses_count'=>23792,
-                'followers_count'=>924
-            ],
-            'authors' => [
-                'LyXww0'
-            ],
-            'document_category' => 'Business_Finance',
-            'canonic_id' => 'Bb_AxyjrEoH',
-            'document_position_source' => 'author',
-            'analyzed' => '2015-03-11T15:27:23.912Z',
-            'partner2' => 'B_PVb',
-            'partner1' => 'KHV7oS',
-            'relationship_indicator' => 'deal',
-            'positive'=>0,
-            'document_url' => 'url:https://twitter.com/FinSentS_SP500/status/575679412219330560',
-            'sentiments'=> [
-                'general_positive'=>0,
-                'violence'=>0,
-                'activism'=>0,
-                'general_negative'=>0,
-                'negative'=>0,
-                'positive'=>0,
-                'profanity'=>0
-            ],
-            'violence'=>0,
-            'original_published' => '2015-03-10T23:25:08.000Z',
-            'document_position'=> [
-                'latitude'=>40.71427,
-                'longitude'=>-74.00597
-            ],
-            'document_external_id' => '575679412219330560',
-            'binning_id' => 'Bb_AxyjrEoH',
-            'entities'=>[
-                'KezJ4l',
-                'url:http://tinyurl.com/pohcymh',
-                'KHV7oS',
-                'KerzW8'
-            ],
-            'meta_type' => 'type:RFEVEOrganizationRelationship',
-            'document_location' => 'B_RYB',
-            'general_positive'=>0,
-            'topics' => [
-                'KPzZAT'
-            ],
-            'negative'=>0,
-            'document_original_id' => '575437259090104320',
-            'fragment_count'=>1
-        ]
-    ];
-
     public function setUp()
     {
         parent::setUp();
-        $this->instance = new Instance($this->fields);
+        $this->instance = new Instance(SingleInstance::get([
+            'id' => 'GUwp9aBfdDn',
+            'type' => 'RFEVEOrganizationRelationship',
+            'fragment' => 'Lorem ipsum dolor sit amet, aliquam sit libero fringilla purus, aliqua ligula.'
+        ])['instances'][0]);
     }
 
     public function function_name_and_expected_data_provider()
@@ -108,8 +28,7 @@ class InstanceTest extends \TestCase
         return [
             ['getId', 'GUwp9aBfdDn'],
             ['getType', 'RFEVEOrganizationRelationship'],
-            ['getFragment', '$has:Us Hasbro in buy area, gives stockholders 2-for-1 deal #Hasbro Inc http://t.co/GZqj2hW9wi #sp500.'],
-            ['getCountry', 'United States'],
+            ['getFragment', 'Lorem ipsum dolor sit amet, aliquam sit libero fringilla purus, aliqua ligula.'],
         ];
     }
 
@@ -131,11 +50,18 @@ class InstanceTest extends \TestCase
 
     public function test_getting_continent_called_the_recorded_future_api()
     {
-        $mockApi = \Mockery::mock(RecordedFutureApi::class);
-        $mockApi->shouldReceive('continentFromCountry')->once()->with('United States');
+        $entity1 = new Entity('123', ['name' => 'Some Name', 'type' => 'Country']);
+        $entity2 = new Entity('123', ['name' => 'Some New Name', 'type' => 'Country']);
+        $entity3 = new Entity('123', ['name' => 'Some New New Name', 'type' => 'Some Other Type']);
 
-        app()->instance(RecordedFutureApi::class, $mockApi);
+        $this->instance->setRelatedEntities([$entity1, $entity3, $entity2]);
 
-        $this->instance->getContinent();
+        $results = $this->instance->getCountries();
+        $this->assertInternalType('array', $results);
+        $this->assertCount(2, $results);
+        $this->assertInstanceOf(Entity::class, $results[0]);
+        $this->assertEquals($entity1->getName(), $results[0]->getName());
+        $this->assertInstanceOf(Entity::class, $results[1]);
+        $this->assertEquals($entity2->getName(), $results[1]->getName());
     }
 }
