8b17b11b642047e80e69b392d37208c030e5f467
Terry Rowland <terry.enolalabs.com>
2016-03-21 09:05:49 -0400

    Added bunch of doc black for accuracy. Changed up how the instances are built, basically having the response figure out what entities belonged on the instance and adding them before giving them back to the requestor. Changed up how the entities are pulled from the response from Guzzle, the wrong key was used. Also removed a function we don't use, so it doesn't need to be tested.

--- app/Services/Vendors/RecordedFuture/Response.php
+++ app/Services/Vendors/RecordedFuture/Response.php
@@ -11,64 +11,91 @@ class Response extends BaseRecord
         $this->record = json_decode($response->getBody(), true);
     }
 
+    /**
+     * Determines if there are more results to be queried from the API.
+     *
+     * @return bool
+     */
     public function hasMorePages(): bool
     {
         return $this->getFieldAsBool('next_page_start');
     }
 
+    /**
+     * Gets the hey used for subsequent queries on queries that have paginated results.
+     *
+     * @return string
+     */
     public function getNextPageStart(): string
     {
         return $this->getFieldAsString('next_page_start');
     }
 
+    /**
+     * Gets the count of records the API returns for the current response.
+     *
+     * @return int
+     */
     public function countOfReferences(): int
     {
         return $this->getFieldAsInt('count.references.returned');
     }
 
+    /**
+     * Gets the total count of records the API will return for a given query. This is the total result count, not the
+     * count that is in the current result set.
+     *
+     * @return int
+     */
     public function totalReferences(): int
     {
         return $this->getFieldAsInt('count.references.total');
     }
 
     /**
+     * Gets the instances from a query.
+     *
      * @return array
      */
     public function getInstances(): array
     {
         $instancesToReturn = [];
+        $requestEntities = $this->getEntities();
         foreach (array_get($this->record, 'instances', []) as $instance) {
-            $instance['entities'] = array_get($this->record, 'entities', []);
-            $instancesToReturn[] = new Instance($instance);
+            $newInstance = new Instance($instance);
+
+            if ($instanceEntityCodes = $newInstance->getAttributes()->getEntityCodes()) {
+                $allInstanceEntities = [];
+                $thisInstanceEntities = array_only($requestEntities, $instanceEntityCodes);
+                foreach ($thisInstanceEntities as $entity) {
+                    $allInstanceEntities = array_merge(
+                        $allInstanceEntities,
+                        array_only($requestEntities, $entity->getContainerCodes())
+                    );
+                }
+
+                $newInstance->setRelatedEntities(array_merge($thisInstanceEntities, $allInstanceEntities));
+            }
+
+            $instancesToReturn[] = $newInstance;
         }
 
         return $instancesToReturn;
     }
 
     /**
+     * Gets the entities from a query.
+     *
      * @return array
      */
     public function getEntities(): array
     {
-        $entities = array_get($this->record, 'entity_details', []);
+        $entities = array_get($this->record, 'entities', []);
         $entitiesToReturn = [];
         foreach ($entities as $entityKey => $entity) {
-            $entitiesToReturn[] = new Entity($entityKey, $entity);
+            $entitiesToReturn[$entityKey] = new Entity($entityKey, $entity);
         }
 
         return $entitiesToReturn;
-    }
-
-    /**
-     * @return Entity
-     */
-    public function getEntity()
-    {
-        $entities = array_get($this->record, 'entity_details', []);
-        foreach ($entities as $entityKey => $entity) {
-            return new Entity($entityKey, $entity);
-        }
-
-        return [];
     }
 }
