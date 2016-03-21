4418e5890c6437d017b4bffe1519910036ee3c94
Terry Rowland <terry.enolalabs.com>
2016-03-21 11:52:40 -0400

    Since the entity_id is required, it needed to be added to the factory as well.

--- database/factories/RegionFactory.php
+++ database/factories/RegionFactory.php
@@ -3,6 +3,7 @@
 $factory->define(App\Entities\Region::class, function (Faker\Generator $faker) {
     return [
         'name' => $faker->name,
+        'entity_id' => '123_QWER',
         'deleted_at' => null,
     ];
 });
