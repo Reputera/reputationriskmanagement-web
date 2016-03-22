92964ad5d76617c98248123fd75996ecc0d93502
Justin Hamm <justin.hamm@enolalabs.com>
2016-03-22 12:09:33 -0400

    Added function to base request to send a builder through pipelines to reduce redundancy.

--- app/Http/Controllers/Instance/QueryController.php
+++ app/Http/Controllers/Instance/QueryController.php
@@ -6,23 +6,29 @@ namespace App\Http\Controllers\Instance;
 use App\Entities\Instance;
 use App\Http\Controllers\Controller;
 use App\Http\Pipelines\Query\SortingPipeline;
+use App\Http\Requests\Request;
 use App\Http\Traits\PaginationTrait;
 use App\Transformers\Instance\InstanceTransformer;
-use Illuminate\Http\Request;
 
 class QueryController extends Controller
 {
     use PaginationTrait;
 
+    /**
+     * @api {get} /instances/ List exams
+     * @apiName ListInstances
+     * @apiDescription List instances based on query parameters. This endpoint is pagination enabled.
+     * @apiGroup Instances
+     * @apiUse MultipleInstances
+     * @apiUse PaginatedResults
+     */
+    /**
+     * @param Request $request
+     * @return \Illuminate\Http\JsonResponse
+     */
     public function getInstances(Request $request)
     {
-        $pipeline = app('Illuminate\Pipeline\Pipeline');
-        $pipeline->send($request);
-        $pipeline->through([SortingPipeline::class]);
-        $builder = Instance::query();
-        $builder = $pipeline->then(function ($request) use ($builder) {
-            return $builder;
-        });
+        $builder = $request->sendBuilderThroughPipeline(Instance::query(), [SortingPipeline::class]);
         return $this->respondWith($builder->get(), new InstanceTransformer());
     }
 }
--- app/Http/Requests/Request.php
+++ app/Http/Requests/Request.php
@@ -2,9 +2,10 @@
 
 namespace App\Http\Requests;
 
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Foundation\Http\FormRequest;
 
-abstract class Request extends FormRequest
+class Request extends FormRequest
 {
     public function authorize()
     {
@@ -22,5 +23,22 @@ abstract class Request extends FormRequest
     public function user($guard = null)
     {
         return parent::user();
+    }
+
+    /**
+     * Send a query builder through specified pipelines.
+     *
+     * @param $builder
+     * @param array $pipelines
+     * @return Builder
+     */
+    public function sendBuilderThroughPipeline(Builder $builder, array $pipelines)
+    {
+        $pipeline = app('Illuminate\Pipeline\Pipeline');
+        $pipeline->send($this);
+        $pipeline->through($pipelines);
+        return $pipeline->then(function ($request) use ($builder) {
+            return $builder;
+        });
     }
 }
