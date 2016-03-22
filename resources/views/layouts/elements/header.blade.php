8096a964ec5fc7ec8bb60d41d6aa67b46836b3ec
Justin Hamm <justin.hamm@enolalabs.com>
2016-03-22 17:53:40 -0400

    Updated blade so error doesn't occur when user isn't logged in.

--- resources/views/layouts/elements/header.blade.php
+++ resources/views/layouts/elements/header.blade.php
@@ -7,13 +7,13 @@
             <ul class="nav navbar-nav">
                 <li class="dropdown user user-menu">
                     <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-                        {{ Auth::user()->name }}
+                        {{ Auth::user()->name ?? '' }}
                     </a>
                     <ul class="dropdown-menu" style="width: 138px">
                         <li class="user-header" style="height: 80px">
                             <p>
-                                {{ Auth::user()->name }}
-                                <small>{{ Auth::user()->email }}</small>
+                                {{ Auth::user()->name ?? ''}}
+                                <small>{{ Auth::user()->email ?? ''}}</small>
                             </p>
                         </li>
                         <li class="user-footer">
