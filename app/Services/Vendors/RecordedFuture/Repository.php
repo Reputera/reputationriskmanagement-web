3e26d41a9b740d3f789cf91130368047c838fe9a
Terry Rowland <terry.enolalabs.com>
2016-03-22 07:49:50 -0400

    Updated the repo to populate the proper columns changed in the migration as well as changed how countries are stored since multiple countries can be stored for an instance.

--- app/Services/Vendors/RecordedFuture/Repository.php
+++ app/Services/Vendors/RecordedFuture/Repository.php
@@ -19,16 +19,6 @@ class Repository
     public function saveInstanceForCompany(Instance $instance, Company $company)
     {
         try {
-            $countryId = null;
-            $regionId = null;
-            if ($countryName = $instance->getCountry()) {
-                $countryInfo = DB::table('countries')->where('name', $countryName)->first(['id', 'region_id']);
-                if ($countryInfo) {
-                    $countryId = $countryInfo->id;
-                    $regionId = $countryInfo->region_id;
-                }
-            }
-
             $vectorId = null;
             if ($vector = Vector::fromEventType($instance->getType())) {
                 $vectorId = $vector->id;
@@ -44,15 +34,13 @@ class Repository
                 $publishedAt = (new Carbon($documentPublishedAt))->toDateTimeString();
             }
 
-            DB::table('instances')->insert([
+            $instanceId = DB::table('instances')->insertGetId([
                 'company_id' => $company->id,
                 'vector_id' => $vectorId,
-                'country_name' => $countryName,
-                'country_id' => $countryId,
-                'region_id' => $regionId,
                 'entity_id' => $instance->getId(),
-                'event_type' => $instance->getType(),
-                'original_language' => $document->getLanguage(),
+                'type' => $instance->getType(),
+                'start' => $publishedAt,
+                'language' => $document->getLanguage(),
                 'source' => $document->getSource()->getName(),
                 'title' => $document->getTitle(),
                 'fragment' => $fragment,
@@ -60,10 +48,23 @@ class Repository
                 'link' => $document->getUrl(),
                 'positive_sentiment' => $attributes->getPositiveSentiment(),
                 'negative_sentiment' => $attributes->getNegativeSentiment(),
-                'published_at' => $publishedAt,
                 'created_at' => $timestamp,
                 'updated_at' => $timestamp
             ]);
+
+            $countryEntityIds = array_map(function ($entity) {
+                return $entity->getId();
+            }, $instance->getCountries());
+
+            if ($instanceId && $countryEntityIds) {
+                $countries = DB::table('countries')->whereIn('entity_id', $countryEntityIds)->get(['id']);
+                foreach ($countries as $country) {
+                    \DB::table('instance_country')->insert([
+                        'instance_id' => $instanceId,
+                        'country_id' => $country->id
+                    ]);
+                }
+            }
         } catch (\Exception $e) {
             $this->error = $e->getMessage();
         }
