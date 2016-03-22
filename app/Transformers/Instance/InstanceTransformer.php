2db75d85ad00637a9e168598209daa203b7a7eaf
Justin Hamm <justin.hamm@enolalabs.com>
2016-03-22 12:09:49 -0400

    Updated instance transformer to explicitly return fields.

--- app/Transformers/Instance/InstanceTransformer.php
+++ app/Transformers/Instance/InstanceTransformer.php
@@ -10,9 +10,20 @@ class InstanceTransformer extends TransformerAbstract
 {
     public function transform(Instance $instance)
     {
-        return (array)$instance;
-//        return [
-//            'id' => $instance->id,
-//        ];
+        return [
+            'id' => $instance->id,
+            'company' => $instance->company->name,
+            'vector' => $instance->vector_id ? $instance->vector->name : null,
+            'type' => $instance->type,
+            'start' => $instance->start,
+            'language' => $instance->language,
+            'source' => $instance->source,
+            'title' => $instance->title,
+            'fragment' => $instance->fragment,
+            'fragment_hash' => $instance->fragment_hash,
+            'link' => $instance->link,
+            'positive_sentiment' => $instance->positive_sentiment,
+            'negative_sentiment' => $instance->negative_sentiment
+        ];
     }
 }
