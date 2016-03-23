7a9c910b077ff3676f84f0a24fce7085be60e1f8
Justin Hamm <justin.hamm@enolalabs.com>
2016-03-23 12:33:29 -0400

    Created web routes.

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -4,6 +4,11 @@ Route::group(['middleware' => ['web']], function () {
     Route::get('login', 'Auth\AuthController@getLogin')->name('login.get');
     Route::post('login', 'Auth\AuthController@postLogin')->name('login.post');
 
+    Route::get('riskScore', 'Instance\QueryController@getRiskScore')->name('instance.getRiskScore');
+    Route::get('instance', 'Instance\QueryController@getInstances')->name('instance.get');
+
+    Route::get('instanceQuery', 'Instance\InstanceUIController@index')->name('instance.index');
+
     Route::post('create-user', 'Users\UserController@store')->name('user.store');
 
     Route::group(['middleware' => ['auth']], function () {
@@ -18,6 +23,7 @@ Route::group(['middleware' => ['web']], function () {
 Route::group(['prefix' => 'api', 'middleware' => ['api']], function () {
     Route::post('login', 'Auth\ApiAuthController@authenticate')->name('api.login.post');
     Route::group(['middleware' => 'auth'], function () {
+//        Instance routes
         Route::get('instance', 'Instance\QueryController@getInstances')->name('instance.get');
         Route::get('riskScore', 'Instance\QueryController@getRiskScore')->name('instance.getRiskScore');
 
