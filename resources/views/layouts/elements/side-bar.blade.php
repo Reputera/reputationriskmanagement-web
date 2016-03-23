2b45f78822196b7b3b9201e1d11ca8733f374fd0
Justin Hamm <justin.hamm@enolalabs.com>
2016-03-23 17:01:17 -0400

    Added link to data query tool to UI.

--- resources/views/layouts/elements/side-bar.blade.php
+++ resources/views/layouts/elements/side-bar.blade.php
@@ -2,6 +2,7 @@
     <section class="sidebar">
         <ul class="sidebar-menu">
             <li class="header">Modules</li>
+            <li><a href="/instanceQuery">Data Query</a></li>
         </ul>
     </section>
 </aside>
