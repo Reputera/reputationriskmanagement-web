28e9646b51fe9269e7745c23d338ca00ee13e9bc
Terry Rowland <terry.enolalabs.com>
2016-03-23 08:34:11 -0400

    Ran the model helper to generate the ons for the instance.

--- app/Entities/Instance.php
+++ app/Entities/Instance.php
@@ -5,6 +5,48 @@ namespace App\Entities;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * App\Entities\Instance
+ *
+ * @property integer $id
+ * @property integer $company_id
+ * @property integer $vector_id
+ * @property string $entity_id
+ * @property string $type
+ * @property string $start
+ * @property string $language
+ * @property string $source
+ * @property string $title
+ * @property string $fragment
+ * @property string $fragment_hash
+ * @property string $link
+ * @property float $sentiment
+ * @property float $positive_sentiment
+ * @property float $negative_sentiment
+ * @property \Carbon\Carbon $created_at
+ * @property \Carbon\Carbon $updated_at
+ * @property-read \App\Entities\Company $company
+ * @property-read \App\Entities\Vector $vector
+ * @property-read \Illuminate\Database\Eloquent\Collection|\App\Entities\Country[] $countries
+ * @method static \Illuminate\Database\Query\Builder|\App\Entities\Instance whereId($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\Entities\Instance whereCompanyId($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\Entities\Instance whereVectorId($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\Entities\Instance whereEntityId($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\Entities\Instance whereType($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\Entities\Instance whereStart($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\Entities\Instance whereLanguage($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\Entities\Instance whereSource($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\Entities\Instance whereTitle($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\Entities\Instance whereFragment($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\Entities\Instance whereFragmentHash($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\Entities\Instance whereLink($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\Entities\Instance whereSentiment($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\Entities\Instance wherePositiveSentiment($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\Entities\Instance whereNegativeSentiment($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\Entities\Instance whereCreatedAt($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\Entities\Instance whereUpdatedAt($value)
+ * @mixin \Eloquent
+ */
 class Instance extends Model
 {
 
