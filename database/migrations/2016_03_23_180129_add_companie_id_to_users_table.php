3204d333ca51676684850ad57f026f9ba83b7bae
Terry Rowland <terry.enolalabs.com>
2016-03-23 14:21:12 -0400

    Added a migration to allow us to tie users to a company. Updated the testing DB as well.

---
+++ database/migrations/2016_03_23_180129_add_companie_id_to_users_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddCompanieIdToUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->unsignedInteger('company_id')->after('email')->nullable();
+
+            $table->foreign('company_id')->references('id')->on('companies');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('company_id');
+        });
+    }
+}
