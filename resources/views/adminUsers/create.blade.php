79240509685d5be7821bb3280e59850c4144d125
Terry Rowland <terry.enolalabs.com>
2016-03-25 07:37:54 -0400

    Fixed a couple issues with the company_id not coming through, resulting in failed signup. Also added some styling to make the page look a little nicer.

--- resources/views/adminUsers/create.blade.php
+++ resources/views/adminUsers/create.blade.php
@@ -11,45 +11,47 @@
         </div>
     @endif
 
-    Create User
-    <form style="width: 20%" method="post" action="{{route('adminUser.store')}}">
-        <input type="hidden" name="_token" value="{{ csrf_token() }}">
-        <div class="form-group">
-            <label for="companySelect">Company</label>
-            <select class="form-control" id="company_id" name="company">
-                @foreach($companies as $company)
-                    <option value="{{$company->is}}" "{{(old('company_id') != $company->id) ? '' : 'selected="selected"'}}">{{$company->name}}</option>
-                @endforeach
-            </select>
-        </div>
-        <div class="form-group">
-            <label for="roleSelect">Role</label>
-            <select class="form-control" id="roleSelect" name="role">
-                @foreach($userRoles as $userRole)
-                    <option value="{{$userRole}}">{{$userRole}}</option>
-                @endforeach
-            </select>
-        </div>
-        <div class="form-group">
-            <label for="name">Name</label>
-            <input type="text" name='name' value="{{old('name')}}" class="form-control" id="name" placeholder="Full Name">
-        </div>
-        <div class="form-group">
-            <label for="userEmail">Email</label>
-            <input type="email" name='email' value="{{old('email')}}" class="form-control" id="userEmail" placeholder="example@domain.com">
-        </div>
-        <div class="form-group">
-            <label for="userPassword">Password</label>
-            <input type="password" name='password' class="form-control" id="userPassword">
-        </div>
-        <div class="form-group">
-            <label for="userPasswordConfirm">Password Confirm</label>
-            <input type="password" name='password_confirmation' class="form-control" id="userPasswordConfirm">
-        </div>
-        <div class="form-group">
-            <div class="col-sm-offset-2 col-sm-10">
-                <button type="submit" class="btn btn-default">Create</button>
+    <div class="col-sm-4">
+        <h3>Create User</h3>
+        <form method="post"  action="{{route('adminUser.store')}}">
+            <input type="hidden" name="_token" value="{{ csrf_token() }}">
+            <div class="form-group">
+                <label for="companySelect">Company</label>
+                <select class="form-control" id="company_id" name="company_id">
+                    @foreach($companies as $company)
+                        <option value="{{$company->id}}" "{{(old('company_id') != $company->id) ? '' : 'selected="selected"'}}">{{$company->name}}</option>
+                    @endforeach
+                </select>
             </div>
-        </div>
-    </form>
+            <div class="form-group">
+                <label for="roleSelect">Role</label>
+                <select class="form-control" id="roleSelect" name="role">
+                    @foreach($userRoles as $userRole)
+                        <option value="{{$userRole}}">{{$userRole}}</option>
+                    @endforeach
+                </select>
+            </div>
+            <div class="form-group">
+                <label for="name">Name</label>
+                <input type="text" name='name' value="{{old('name')}}" class="form-control" id="name" placeholder="Full Name">
+            </div>
+            <div class="form-group">
+                <label for="userEmail">Email</label>
+                <input type="email" name='email' value="{{old('email')}}" class="form-control" id="userEmail" placeholder="example@domain.com">
+            </div>
+            <div class="form-group">
+                <label for="userPassword">Password</label>
+                <input type="password" name='password' class="form-control" id="userPassword">
+            </div>
+            <div class="form-group">
+                <label for="userPasswordConfirm">Password Confirm</label>
+                <input type="password" name='password_confirmation' class="form-control" id="userPasswordConfirm">
+            </div>
+            <div class="form-group">
+                <div class="text-center">
+                    <button type="submit" class="btn btn-default">Create</button>
+                </div>
+            </div>
+        </form>
+    </div>
 @endsection
