b115944c160405a1884382941ea4d6090dd3b632
Terry Rowland <terry.enolalabs.com>
2016-03-25 07:25:58 -0400

    Added a link to the user creation page.

--- resources/views/layouts/elements/side-bar.blade.php
+++ resources/views/layouts/elements/side-bar.blade.php
@@ -2,7 +2,10 @@
     <section class="sidebar">
         <ul class="sidebar-menu">
             <li class="header">Modules</li>
-            <li><a href="/instanceQuery">Data Query</a></li>
+            @if(Auth::user()->isAdmin())
+                <li><a href="{{route('adminUser.get')}}">Create User</a></li>
+            @endif
+            <li><a href="{{route('instance.index')}}">Data Query</a></li>
         </ul>
     </section>
 </aside>
