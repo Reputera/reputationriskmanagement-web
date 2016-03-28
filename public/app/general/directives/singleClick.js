41022408acbebc3accf60a5fdc2755c186454845
Justin Hamm <justin.hamm@enolalabs.com>
2016-03-28 18:00:48 -0400

    Pull in/update angular classes

---
+++ public/app/general/directives/singleClick.js
@@ -0,0 +1,28 @@
+angular.module('singleClick', []).directive('singleClick', function ($timeout) {
+    var delay = 1000;   // min milliseconds between clicks
+
+    return {
+        restrict: 'A',
+        priority: -1,   // cause out postLink function to execute before native `ngClick`'s
+                        // ensuring that we can stop the propagation of the 'click' event
+                        // before it reaches `ngClick`'s listener
+        link: function (scope, elem) {
+            var disabled = false;
+
+            function onClick(evt) {
+                if (disabled) {
+                    evt.preventDefault();
+                    evt.stopImmediatePropagation();
+                } else {
+                    disabled = true;
+                    $timeout(function () {
+                        disabled = false;
+                    }, delay, false);
+                }
+            }
+
+            scope.$on('$destroy', function () { elem.off('click', onClick); });
+            elem.on('click', onClick);
+        }
+    };
+});
