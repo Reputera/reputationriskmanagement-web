14a9ac847743416dcec2b5bfbfdc83c7e1d912d9
Justin Hamm <justin.hamm@enolalabs.com>
2016-03-29 16:42:09 -0400

    Created route to flag instances.

---
+++ app/Http/Controllers/Instance/InstanceController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers\Instance;
+
+use App\Entities\Instance;
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Request;
+
+class InstanceController extends Controller
+{
+
+    public function flag(Request $request)
+    {
+        Instance::where('id', $request->get('id'))
+            ->firstOrFail()
+            ->setFlagged($request->get('flagged'))
+            ->save();
+    }
+}
