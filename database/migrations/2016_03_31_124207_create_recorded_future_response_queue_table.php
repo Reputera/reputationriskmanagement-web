e040723060f12da9914eb0c8d20ae7b6205e553b
Terry Rowland <terry.enolalabs.com>
2016-03-31 09:20:08 -0400

    Created the table that will hold the responses from Recorded Future to be processed at a later time.

---
+++ database/migrations/2016_03_31_124207_create_recorded_future_response_queue_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateRecordedFutureResponseQueueTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('recorded_future_response_queue', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('company_id');
+            $table->longText('response');
+
+            $table->foreign('company_id')->references('id')->on('companies');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('recorded_future_response_queue');
+    }
+}
