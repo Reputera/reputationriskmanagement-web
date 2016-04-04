98fcf983da79714787bbe6d158ad457bdac260f2
Justin Hamm <justin.hamm@enolalabs.com>
2016-04-04 12:21:12 -0400

    Added date and region columns to output table. Set defaults for vector and region to all to explicitly tell the user the query will search on anything for these fields.

--- resources/views/instance/instanceQuery.blade.php
+++ resources/views/instance/instanceQuery.blade.php
@@ -4,17 +4,17 @@
     <div ng-controller="InstanceQueryController">
         <label>Company</label>
         <select class="form-control" ng-model="selectedCompany" ng-options="company.name for company in companies">
-            <option value=""></option>
+            <option value="">Select a Company</option>
         </select>
 
         <label>Vector</label>
         <select class="form-control" ng-model="selectedVector" ng-options="vector.name for vector in vectors">
-            <option value=""></option>
+            <option value="">All Vectors</option>
         </select>
 
         <label>Region</label>
         <select class="form-control" ng-model="selectedRegion" ng-options="region.name for region in regions">
-            <option value=""></option>
+            <option value="">All Regions</option>
         </select>
 
         <label>Fragment Search</label>
@@ -36,12 +36,15 @@
             <p>Instance count: <span ng-bind="resultCount"></span></p>
         </div>
         <div ng-if="riskScore">
+            <a class="btn btn-primary" target="_blank" ng-href="@{{csvUrl}}">Download CSV</a>
             <table ng-cloak ng-table="instanceTable" class="table table-striped table-hover">
-                <tr ng-repeat="instance in $data track by instance.title">
+                <tr ng-repeat="instance in $data track by instance.id">
                     <td data-title="'Title'" sortable="'user_name'" ng-bind="instance.title"></td>
+                    <td data-title="'Date'" sortable="'date'" ng-bind="instance.date"></td>
                     <td data-title="'Vector'" ng-bind="instance.vector"></td>
                     <td data-title="'Risk Score'" ng-bind="instance.risk_score"></td>
                     <td data-title="'Source'" ng-bind="instance.source"></td>
+                    <td data-title="'Region'" ng-bind="instance.regions"></td>
                     <td data-title="'Fragment'" ng-bind="instance.fragment"></td>
                     <td data-title="'Flagged'">
                         <button class="btn btn-primary" ng-if="instance.flagged" ng-click="flag(instance.id, 0)">Unflag</button>
