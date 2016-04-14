357d6b44ac7f8a56c92d5d41a66a9428447221f9
Terry Rowland <terry.enolalabs.com>
2016-04-14 14:55:29 -0400

    Updated the user factory to create companies as needed. Any user or user account admin should be tied to a company.

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Entities\Company;
 use App\Entities\Role;
 
 $factory->define(App\Entities\User::class, function (Faker\Generator $faker) {
@@ -8,18 +9,37 @@ $factory->define(App\Entities\User::class, function (Faker\Generator $faker) {
         'email' => $faker->email,
         'password' => bcrypt(str_random(10)),
         'remember_token' => str_random(10),
-        'role' => Role::USER
+        'role' => Role::USER,
+        'company_id' => null
     ];
 });
 
 $factory->defineAs(App\Entities\User::class, 'admin', function ($faker) use ($factory) {
     $user = $factory->raw(App\Entities\User::class);
 
-    return array_merge($user, ['role' => Role::ADMIN]);
+    return array_merge($user, [
+        'role' => Role::ADMIN,
+    ]);
+});
+
+$factory->defineAs(App\Entities\User::class, 'user', function ($faker) use ($factory) {
+    $user = $factory->raw(App\Entities\User::class);
+
+    return array_merge($user, [
+        'role' => Role::ADMIN,
+        'company_id' => function () {
+            return factory(Company::class)->create()->id;
+        },
+    ]);
 });
 
 $factory->defineAs(App\Entities\User::class, 'user_admin', function ($faker) use ($factory) {
     $user = $factory->raw(App\Entities\User::class);
 
-    return array_merge($user, ['role' => Role::USER_ACCOUNT_ADMIN]);
+    return array_merge($user, [
+        'role' => Role::USER_ACCOUNT_ADMIN,
+        'company_id' => function () {
+            return factory(Company::class)->create()->id;
+        },
+    ]);
 });
