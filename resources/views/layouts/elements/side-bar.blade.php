842f57274e03d7486294e6af3d657494509c14c7
Terry Rowland <terry.enolalabs.com>
2016-04-20 09:30:19 -0400

    Updated the links on the sidebar to have both the company management and user management and be collapsible since there will be multiple links for each category (future).

--- resources/views/layouts/elements/side-bar.blade.php
+++ resources/views/layouts/elements/side-bar.blade.php
@@ -3,10 +3,21 @@
         <ul class="sidebar-menu">
             <li class="header">Modules</li>
             @if(Auth::user()->isAdmin())
-                <li><a href="{{route('adminUser.get')}}">Create User</a></li>
+                <li class="treeview">
+                    <a href="#"><span>User Management</span> <i class="fa fa-angle-left pull-right"></i></a>
+                    <ul class="treeview-menu">
+                        <li><a href="{{route('adminUser.get')}}">Create User</a></li>
+                    </ul>
+                </li>
+                <li class="treeview">
+                    <a href="#"><span>Company Management</span> <i class="fa fa-angle-left pull-right"></i></a>
+                    <ul class="treeview-menu">
+                        <li><a href="{{route('companyCreate.get')}}">Create Company</a></li>
+                    </ul>
+                </li>
+                <li><a href="{{route('instance.index')}}">Data Query</a></li>
+                <li><a href="/sentimentQuery">Sentiment Query</a></li>
             @endif
-            <li><a href="{{route('instance.index')}}">Data Query</a></li>
-            <li><a href="/sentimentQuery">Sentiment Query</a></li>
         </ul>
     </section>
 </aside>
