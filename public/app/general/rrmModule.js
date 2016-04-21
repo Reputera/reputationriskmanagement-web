ed181dc80ff40f14fa0072b79bda4a9ddf0c84f0
Terry Rowland <terry.enolalabs.com>
2016-04-21 16:21:15 -0400

    Created a way to turn off the global error capturing. I needed to control how validation errors were returned and the toastr messages to be used when creating companies.

--- public/app/general/rrmModule.js
+++ public/app/general/rrmModule.js
@@ -9,32 +9,42 @@ var app = angular
         'singleClick',
         'angularModalService'
     ])
-    .factory('errorInterceptor', function($q, toastr) {
+    .factory('ApplicationConfig', function($log) {
+        return {
+            enableGlobalErrorHandling: true
+        };
+    })
+    .factory('errorInterceptor', function($q, toastr, ApplicationConfig) {
         return {
             responseError: function (response) {
-                if (response.status == 404) {
-                    toastr.warning('Record not found');
-                } else if (response.status == 500) {
-                    toastr.error('Unknown error occurred');
-                } else if(response.status == 400) {
-                    toastr.error(response.data.message);
-                } else if(response.status == 401) {
-                    toastr.error('Unauthorized: ' + response.data.message);
-                } else if (response.status == 422) {
-                    if (response.data.hasOwnProperty('errors')) {
-                        var errors = '';
-                        for (var error_field in response.data.errors) {
-                            errors += errors + response.data.errors[error_field] + "\n";
+                if(ApplicationConfig.enableGlobalErrorHandling) {
+                    if (response.status == 404) {
+                        toastr.warning('Record not found');
+                    } else if (response.status == 500) {
+                        toastr.error('Unknown error occurred');
+                    } else if (response.status == 400) {
+                        toastr.error(response.data.message);
+                    } else if (response.status == 401) {
+                        toastr.error('Unauthorized: ' + response.data.message);
+                    } else if (response.status == 422) {
+                        if (response.data.hasOwnProperty('errors')) {
+                            var errors = '';
+                            for (var error_field in response.data.errors) {
+                                errors += errors + response.data.errors[error_field] + "\n";
+                            }
+                            try {
+                                toastr.warning(errors);
+                            } catch (e) {
+                            }
+                        } else {
+                            try {
+                                toastr.warning(JSON.stringify(response.data));
+                            } catch (e) {
+                            }
                         }
-                        try {
-                            toastr.warning(errors);
-                        } catch(e) {}
-                    } else {
-                        try {
-                            toastr.warning(JSON.stringify(response.data));
-                        } catch(e) {}
                     }
                 }
+
                 return $q.reject(response);
             }
         }
