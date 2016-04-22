97054cddf51a957dfde20fd9648e277ef1b1af35
Terry Rowland <terry.enolalabs.com>
2016-04-22 08:31:47 -0400

    Updated the company creation blade to accommodate a per-company error style, along with actually saving the companies provided.

--- resources/views/company/create.blade.php
+++ resources/views/company/create.blade.php
@@ -12,40 +12,56 @@
                 <th>Entity ID (Recorded Future)</th>
                 <th>Industry</th>
             </tr>
-            <tr ng-repeat="company in companiesToAdd">
-                <td>
+            <tr ng-repeat="(companyKey, company) in companiesToAdd">
+                <td style="height: 90px;">
                     <button type="button" ng-if="hasMultipleCompanies()"
                             class="btn btn-danger" ng-click="removeCompany(company.id);"
                     >
                         x
                     </button>
                 </td>
-                <td>
-                    <input type="text" class="form-control" ng-model="company.name"
-                           name="company_name@{{company.id}}"
-                    >
-                </td>
-                <td>
-                    <input type="text" class="form-control"
-                           ng-model="company.entity_id" name="company_entity_id@{{company.id}}"
-                           placeholder="Recorded Future Entity Id"
-                    >
-                </td>
-                <td>
-                    <select >
-                        <option>Select Industry</option>
-                        <option ng-repeat="industry in industries"
-                                value="@{{ industry.id }}"
+                <td style="height: 90px;">
+                    <div class="form-group" ng-class="{ 'has-error' : companiesToAddErrors[companyKey]['name'] }">
+                        <input type="text" class="form-control" ng-model="company.name"
+                               required
+                               name="company_name@{{company.id}}"
                         >
-                            @{{ industry.name }}
-                        </option>
-                    </select>
+                        <div class="control-label" ng-if="companiesToAddErrors[companyKey]['name']">
+                            <strong ng-bind='companiesToAddErrors[companyKey]["name"]'></strong>
+                        </div>
+                    </div>
+                </td>
+                <td style="height: 90px;">
+                    <div class="form-group" ng-class="{ 'has-error' : companiesToAddErrors[companyKey]['entity_id'] }">
+                        <input type="text" class="form-control" required
+                               ng-model="company.entity_id"
+                               placeholder="Recorded Future Entity Id"
+                        >
+                        <div class="control-label" ng-if="companiesToAddErrors[companyKey]['entity_id']">
+                            <strong ng-bind='companiesToAddErrors[companyKey]["entity_id"]'></strong>
+                        </div>
+                    </div>
+                </td>
+                <td>
+                    <div class="form-group" ng-class="{ 'has-error' : companiesToAddErrors[companyKey]['industry_id'] }">
+                        <select ng-model="company.industry_id" required>
+                            <option value="" selected="selected">Select Industry</option>
+                            <option ng-repeat="industry in industries"
+                                    value="@{{ industry.id }}"
+                            >
+                                @{{ industry.name }}
+                            </option>
+                        </select>
+                        <div class="control-label" ng-if="companiesToAddErrors[companyKey]['industry_id']">
+                            <strong ng-bind='companiesToAddErrors[companyKey]["industry_id"]'></strong>
+                        </div>
+                    </div>
                 </td>
             </tr>
         </table>
         <button type="button" class="btn btn-primary" ng-click="addNewCompany()">Add Another Company</button>
         <br /><br />
-        <button type="button" class="btn btn-success" ng-click="">Save Companies</button>
+        <button type="button" class="btn btn-success" ng-click="saveCompanies()" ng-click="">Save Companies</button>
     </div>
 @endsection
 
@@ -71,14 +87,19 @@
 </script>
 
 <script>
-    app.controller('CompanyCreateController', ['$scope', '$http', 'toastr', 'ModalService', function($scope, $http, toastr, ModalService) {
+    app.controller('CompanyCreateController',
+            ['$scope', '$http', 'toastr', 'ModalService', 'ApplicationConfig', function($scope, $http, toastr, ModalService, ApplicationConfig)
+    {
+        ApplicationConfig.enableGlobalErrorHandling = false;
+
         $scope.companiesToAdd = [];
+        $scope.companiesToAddErrors = [];
         $scope.industries = industries;
         $scope.newIndustry = [];
 
         $scope.addNewCompany = function() {
             var newCompanyTempId = $scope.companiesToAdd.length+1;
-            $scope.companiesToAdd.push({'id': newCompanyTempId, 'name': '', 'entity_id': ''});
+            $scope.companiesToAdd.push({'id': newCompanyTempId, 'name': '', 'entity_id': '', 'industry_id': ''});
         };
 
         $scope.removeCompany = function(companyId) {
@@ -100,6 +121,36 @@
                     $scope.industries.push(result);
                 });
             });
+        };
+
+        $scope.saveCompanies = function () {
+            $scope.companiesToAddErrors = [];
+            $http({
+                method: 'POST',
+                url: 'api/create-company',
+                data: {
+                    companies: $scope.companiesToAdd
+                }
+            })
+            .then(
+                function success(response) {
+                    toastr.success('Successfully added the companies!');
+                    $scope.companiesToAdd = [];
+                    $scope.addNewCompany();
+                },
+                function error(response) {
+                    toastr.error('There were issues with saving the companies.');
+                    if (response.data.hasOwnProperty('errors')) {
+                        for (var error_field in response.data.errors) {
+                            var field = error_field.split(".");
+                            if(typeof $scope.companiesToAddErrors[field[1]] == "undefined"){
+                                $scope.companiesToAddErrors[field[1]] = [];
+                            }
+                            $scope.companiesToAddErrors[field[1]][field[2]] = response.data.errors[error_field][0];
+                        }
+                    }
+                }
+            )
         };
 
         $scope.hasMultipleCompanies = function () {
@@ -132,5 +183,6 @@
             });
         };
     });
+
 </script>
 @endsection
