b83ea87dc2a785cea53625c8e7ac93a6bae0ac60
Terry Rowland <terry.enolalabs.com>
2016-05-13 10:38:48 -0400

    Created a based email layout for emails.

---
+++ resources/views/mail/baseMail.blade.php
@@ -0,0 +1,51 @@
+<html>
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <title>Reputera</title>
+</head>
+<body leftmargin="0" marginwidth="0" topmargin="0" marginheight="0" offset="0">
+<div style="margin-top:200px;background-color:#f0f0f1;width:100%;-webkit-text-size-adjust:none !important;margin:0;">
+    <table border="0" cellpadding="0" cellspacing="0" height="100%" width="100%">
+        <tr>
+            <td align="center" valign="top">
+                <table border="0" cellpadding="0" cellspacing="0" id="template_container" style="-webkit-box-shadow:0 0 0 3px rgba(0,0,0,0.025) !important;box-shadow:0 0 0 3px rgba(0,0,0,0.025) !important;-webkit-border-radius:6px !important;border-radius:8px !important;background-color: #fdfdfd;border: 1px solid #c0c0c1;-webkit-border-radius:8px !important;border-radius:8px !important;">
+                    <tr>
+                        <td align="center" valign="top">
+                            <table border="0" cellpadding="0" cellspacing="0" width="600" id="template_header" style="background-color:#09469B;color: #ffffff;-webkit-border-top-left-radius:8px !important;-webkit-border-top-right-radius:8px !important;border-top-left-radius:8px !important;border-top-right-radius:8px !important;border-bottom: 0;font-family:Arial;font-weight:bold;line-height:100%;vertical-align:middle;" bgcolor="#09469B">
+                                <tr>
+                                    <td>
+                                        <h2 class="text-center" align="center" style="color: white;margin:10px;">
+                                            Reputera
+                                        </h2>
+                                    </td>
+                                </tr>
+                            </table>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td align="center" valign="top">
+                            <table border="0" cellpadding="0" cellspacing="0" width="600" id="template_body">
+                                <tr>
+                                    <td valign="top" style="background-color:#fdfdfd;-webkit-border-radius:8px !important;border-radius:8px !important;">
+                                        <table border="0" cellpadding="20" cellspacing="0" width="100%">
+                                            <tr>
+                                                <td valign="top">
+                                                    <div style="color: #737373;font-family:Arial;font-size:14px;line-height:150%;text-align:left; margin: 0 10px;">
+                                                        @yield('content')
+                                                    </div>
+                                                </td>
+                                            </tr>
+                                        </table>
+                                    </td>
+                                </tr>
+                            </table>
+                        </td>
+                    </tr>
+                </table>
+                <p style="text-align:center;font-family:sans-serif;font-size:12px;"><a target="#" href="{{ url('/') }}" style="text-decoration: none;">Reputera</a></p>
+            </td>
+        </tr>
+    </table>
+</div>
+</body>
+</html>
