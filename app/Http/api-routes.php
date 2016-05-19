9c96bddac12dcf54d16cbd773bdb8237ef2a09b4
Justin Hamm <justin.hamm@enolalabs.com>
2016-05-19 17:29:13 -0400

    Updated middleware to new role middleware to resolve error.

--- app/Http/api-routes.php
+++ app/Http/api-routes.php
@@ -4,7 +4,7 @@ Route::group(['prefix' => 'api', 'middleware' => ['api']], function () {
     Route::post('login', 'Auth\ApiAuthController@authenticate')->name('api.login.post');
     Route::post('password/reset', 'Auth\ApiPasswordController@sendResetLinkEmail')->name('api.password.resetToken.post');
 
-    Route::group(['middleware' => ['auth', 'adminAccess']], function () {
+    Route::group(['middleware' => ['auth', 'role:'.\App\Entities\Role::ADMIN]], function () {
         Route::get('vector-risk-scores-by-month', 'Vector\MonthlyRiskScoreController@byCompany')
             ->name('instance.getMonthlyVectorComparison');
 //        Instance routes
