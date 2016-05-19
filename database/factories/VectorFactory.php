20d67cb5f356ddc03c2f2c0ca371ed2ec65ffa1e
Justin Hamm <justin.hamm@enolalabs.com>
2016-05-19 18:52:03 -0400

    Implemented company vector colors to define which colors should be displayed for a vector in a particular company.

--- database/factories/VectorFactory.php
+++ database/factories/VectorFactory.php
@@ -3,5 +3,6 @@
 $factory->define(App\Entities\Vector::class, function (Faker\Generator $faker) {
     return [
         'name' => $faker->name,
+        'default_color' => $faker->hexColor
     ];
 });
---
+++ database/migrations/2016_05_17_151434_add_default_color_to_vectors_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddDefaultColorToVectorsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('vectors', function (Blueprint $table) {
+            $table->string('default_color')->nullable()->after('name');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('vectors', function (Blueprint $table) {
+            $table->dropColumn('default_color');
+        });
+    }
+}
