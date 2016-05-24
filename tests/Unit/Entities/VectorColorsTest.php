743484955ef51c47d52eae1353d216d03fd7479f
Justin Hamm <justin.hamm@enolalabs.com>
2016-05-24 15:27:23 -0400

    Remove test for removed functionality.

--- tests/Unit/Entities/VectorColorsTest.php
+++ tests/Unit/Entities/VectorColorsTest.php
@@ -8,19 +8,6 @@ use App\Entities\Vector;
 
 class GettingVectorsPerMonthTest extends \TestCase
 {
-    public function testGetColor()
-    {
-        $vector = factory(Vector::class)->create();
-        $user = $this->beLoggedInAsUser();
-        $companyVectorColor = CompanyVectorColor::create([
-            'company_id' => $user->company_id,
-            'vector_id' => $vector->id,
-            'color' => str_random(5)
-        ]);
-
-        $this->assertEquals($companyVectorColor->color, $vector->color());
-    }
-
     public function testGetColorDefault()
     {
         $vector = factory(Vector::class)->create();
