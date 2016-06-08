8886da7e714a095dc8541e489de9907922e8b62f
Justin Hamm <justin.hamm@enolalabs.com>
2016-06-08 18:16:03 -0400

    Fixed typo in config.

--- config/rrm.php
+++ config/rrm.php
@@ -3,7 +3,7 @@ return [
     'filesystem' => [
         'logo' => [
             'directory' => 'logos',
-            'fileystem' => env('LOGO_FILESYSTEM', 'local'),
+            'filesystem' => env('LOGO_FILESYSTEM', 'local'),
             'max_width' => 200,
             'max_height' => 100
         ]
