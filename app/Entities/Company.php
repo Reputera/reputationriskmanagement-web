e9ca7627f6371fef44244ddeddb3651d99f560c1
Terry Rowland <terry.enolalabs.com>
2016-06-08 07:37:20 -0400

    Since I've offloaded the work to a class, the test changes were to use mocking since the actual class where the work is contains the non mocked work.

--- app/Entities/Company.php
+++ app/Entities/Company.php
@@ -4,11 +4,11 @@ namespace App\Entities;
 
 use App\Http\Queries\Instance as InstanceQuery;
 use App\Http\QueryFilter;
+use App\Services\Instance\ReputationChange;
 use App\Services\Vendors\RecordedFuture\InstanceApiResponseQueue;
 use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
-use Illuminate\Support\Facades\DB;
 
 /**
  * App\Entities\Company
@@ -49,6 +49,11 @@ class Company extends Model
     public function industries()
     {
         return $this->belongsToMany(Industry::class);
+    }
+
+    public function instances()
+    {
+        return $this->hasMany(Instance::class);
     }
 
     public function competitors()
@@ -115,50 +120,27 @@ class Company extends Model
     }
 
     /**
-     * Gets the reputation change for a particular company between two dates. The way this is done is rather complex.
+     * Gets the reputation change for a particular company between two dates.
      *
-     * @link http://www.csgnetwork.com/percentchangecalc.html
-     * @link http://math.stackexchange.com/questions/716767/how-to-calculate-the-percentage-of-increase-decrease-with-negative-numbers
      * @param Carbon $start
      * @param Carbon $end
      * @return float
      */
-    public function reputationChangeByDate(Carbon $start, Carbon $end): float
+    public function reputationChangeBetweenDates(Carbon $start, Carbon $end): float
     {
-        // We initially need to make a query to populate the instance scores (reputation score) grouped by the
-        // date (YYYY-MM-DD) of the start column. They way the the datetimes (dates withe hours/minutes/seconds)
-        // do not try to group.
-        $builder = (new Instance)->dailyCompanyRiskScore($this)
-            ->whereRaw("start between '{$start->toDateString()} 00:00:00' and '{$end->toDateString()} 23:59:59'")
-            ->orderByRaw('start_date ASC');
+        return app(ReputationChange::class)->forCompanyBetween($this, $start, $end);
+    }
 
-        $results = DB::select("{$builder->toSql()}", $builder->getBindings());
-
-        $finalScores = [];
-        foreach ($results as $key => $result) {
-            if (!array_key_exists($key + 1, $results)) {
-                continue;
-            }
-
-            $originalNumber = $result->company_risk_scores;
-            $secondNumber = $results[$key + 1]->company_risk_scores;
-            if (($originalNumber - $secondNumber) > 0) {
-                $value = ($originalNumber - $secondNumber) / $originalNumber;
-            } else {
-                $value = ($secondNumber - $originalNumber) / $originalNumber;
-            }
-
-            if ($originalNumber > $secondNumber && ($originalNumber > 0)) {
-                $value = -1 * $value;
-            }
-
-            $finalScores[] = $value * 100;
-        }
-
-        if (!$finalScores) {
-            return 0.0;
-        }
-
-        return (float) (array_sum($finalScores) / count($finalScores));
+    /**
+     * Gets the reputation change for a particular company between two dates.
+     *
+     * @param Region $region
+     * @param Carbon $start
+     * @param Carbon $end
+     * @return float
+     */
+    public function reputationChangeForRegionBetweenDates(Region $region, Carbon $start, Carbon $end): float
+    {
+        return app(ReputationChange::class)->forCompanyAndRegionBetween($this, $region, $start, $end);
     }
 }
--- tests/Unit/Entities/CompanyTest.php
+++ tests/Unit/Entities/CompanyTest.php
@@ -3,7 +3,8 @@
 namespace Tests\Unit\Entities;
 
 use App\Entities\Company;
-use App\Entities\Instance;
+use App\Entities\Region;
+use App\Services\Instance\ReputationChange;
 use App\Services\Vendors\RecordedFuture\InstanceApiResponseQueue;
 use Carbon\Carbon;
 
@@ -43,32 +44,44 @@ class CompanyTest extends \TestCase
         $company->queueInstancesDaily($cadence);
     }
 
-    public function testReputationChangeByDateWhenRiskScoresAreMissingForDates()
+    public function testGettingReputationChangeForCompanyBetweenDates()
     {
+        $mockedReputationChange = \Mockery::mock(ReputationChange::class);
+
+        $dateOne = Carbon::now();
+        $dateTwo = Carbon::now();
+        $dateTwo->addDay();
+        $mockedReputationChange->shouldReceive('forCompanyBetween')
+            ->once()
+            ->with(\Mockery::type(Company::class), $dateOne, $dateTwo)
+            ->andReturn(0.00);
+
+        app()->instance(ReputationChange::class, $mockedReputationChange);
+
         /** @var Company $company */
         $company = factory(Company::class)->create();
-        $startDate = Carbon::create(2016, 05, 01);
-        $endDate = Carbon::create(2016, 05, 05);
 
-        // 05-01
-        factory(Instance::class)->create(['company_id' => $company->id, 'risk_score' => 31, 'start' => $startDate->toDateTimeString()]);
-        factory(Instance::class)->create(['company_id' => $company->id, 'risk_score' => 47, 'start' => $startDate->toDateTimeString()]);
-        factory(Instance::class)->create(['company_id' => $company->id, 'risk_score' => 59, 'start' => $startDate->toDateTimeString()]);
-        // 45.6667 - 43.3333 = 89.000 / 45.6667 = 1.9489 = -194.89%
+        $company->reputationChangeBetweenDates($dateOne, $dateTwo);
+    }
 
-        // 05-03
-        $secondDay = clone $startDate;
-        $secondDay->addDays(2);
-        factory(Instance::class)->create(['company_id' => $company->id, 'risk_score' => -52, 'start' => $secondDay->toDateTimeString()]);
-        factory(Instance::class)->create(['company_id' => $company->id, 'risk_score' => -68, 'start' => $secondDay->toDateTimeString()]);
-        factory(Instance::class)->create(['company_id' => $company->id, 'risk_score' => -10, 'start' => $secondDay->toDateTimeString()]);
-        // -43.3333 - -52.0000 = -8.666 / -43.3333 = .20 = -20%
+    public function testGettingReputationChangeForCompanyForRegionBetweenDates()
+    {
+        $mockedReputationChange = \Mockery::mock(ReputationChange::class);
 
-        // 05-05
-        // These shouldn't count since we can't compare to a "next day"
-        factory(Instance::class)->create(['company_id' => $company->id, 'risk_score' => -52, 'start' => $endDate->toDateTimeString()]);
+        $dateOne = Carbon::now();
+        $dateTwo = Carbon::now();
+        $dateTwo->addDay();
+        $mockedReputationChange->shouldReceive('forCompanyAndRegionBetween')
+            ->once()
+            ->with(\Mockery::type(Company::class), \Mockery::type(Region::class), $dateOne, $dateTwo)
+            ->andReturn(0.00);
 
-        // -194.89 + -20 = 214 / 2 = 107.445
-        $this->assertEquals('-107.445', number_format($company->reputationChangeByDate($startDate, $endDate), 3));
+        app()->instance(ReputationChange::class, $mockedReputationChange);
+
+        /** @var Company $company */
+        $company = factory(Company::class)->create();
+        $region = factory(Region::class)->create();
+
+        $company->reputationChangeForRegionBetweenDates($region, $dateOne, $dateTwo);
     }
 }
