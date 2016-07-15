c97cfebffc98470bbb692cb96aa946d04d52e88e
Justin Hamm <justin.hamm@enolalabs.com>
2016-07-15 13:29:10 -0400

    Updated the address where mails are sent form to be configurable.

--- config/mail.php
+++ config/mail.php
@@ -54,7 +54,7 @@ return [
     |
     */
 
-    'from' => ['address' => 'donotreply@reputera.com', 'name' => 'Reputera'],
+    'from' => ['address' => env('FROM_EMAIL', 'donotreply@reputera.com') , 'name' => 'Reputera'],
 
     /*
     |--------------------------------------------------------------------------
